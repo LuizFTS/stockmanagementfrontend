56b3dedc3586d5a950d620c39e664e7cc64a3fa5
Luiz Filipe Tosta da Silva <luizftosta@outlook.com>
2025-11-11 21:22:37 -0300

    style: started to configure the style files and variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('stockmanagementfrontend');
+
+  ngOnInit() {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    this.setTheme(mediaQuery.matches);
+
+    mediaQuery.addEventListener('change', (e) => {
+      this.setTheme(e.matches);
+    });
+  }
+  private setTheme(prefersDark: boolean): void {
+    if (prefersDark) {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
 }
